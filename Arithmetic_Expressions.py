2d5012bd802e36691b295992098c7b1aed16d328
SwarajPawarV2Solutions <swaraj.pawar@v2solutions.com>
2024-09-10 17:10:10 +0530

    Added Expression and Data types

---
+++ Arithmetic_Expressions.py
@@ -0,0 +1,8 @@
+print (2 + 2)
+print (5 - 2)
+print (10 / 2)
+print (2 * 10)
+print (5 // 2)
+print (10 % 2)
+print (2 ** 2)
+print (2 + 2 * 10)
---
+++ Data_types.py
@@ -0,0 +1,15 @@
+A = 10
+b = 6.9
+c ="hello"
+
+print (type(A))
+print (type(b))
+print (type(c))
+
+# String concatination
+
+print ("hello" + "world")
+
+ #String Replication
+ 
+print ("hello" * 5)  # It will print hello 5 times
