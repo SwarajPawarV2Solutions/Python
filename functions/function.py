951687bd1e080a0cff03206b94b02e383e6b8ed7
SwarajPawarV2Solutions <swaraj.pawar@v2solutions.com>
2024-09-26 16:46:28 +0530

    Adder Functions folder

---
+++ functions/function.py
@@ -0,0 +1,24 @@
+# Function
+
+def hello():
+    print('Hey')
+    print('Hey There')
+    
+
+hello() # calling Function
+
+
+
+def greeting(name):
+    print('Hello ' + name)
+    
+
+
+greeting('swaraj')
+
+def addition(num1,num2):
+    results = num1 + num2
+    print(results)
+    
+
+addition(500,500)
