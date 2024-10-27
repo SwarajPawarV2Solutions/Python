9c21007d5f96a4279d26139fcd34063c7e4dc7a5
SwarajPawarV2Solutions <swaraj.pawar@v2solutions.com>
2024-10-27 13:19:03 +0530

    Added Iterators.py in Advanced python

--- Advanced Python/iterators.py
+++ Advanced Python/iterators.py
@@ -1,3 +1,24 @@
 # Iterator is an object that contains a countable number of values
 # An iterator is an object that can be iterated upon, meaning that you can traversethrough all the values.
 # technically, in python,an iterator is an object which permmits  the iterator protocol, which consists of the methods _iter_() and _next_().
+
+
+# Iterators
+
+for i in range(10):
+    print(i,end=' ')
+    
+print()
+
+for n in [2,4,6,8]:
+    # do some operations
+    print(n+1,end=' ')
+
+print()    
+print(iter([2,4,6,8,]))
+
+N = 10**12
+for i in range(N):
+    if i>=10:
+        break
+    print(i,end=' ')
