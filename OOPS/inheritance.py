bb51b88c9503910a246754a7678ef5d4bf15d34d
SwarajPawarV2Solutions <swaraj.pawar@v2solutions.com>
2024-10-28 11:18:41 +0530

    Added Inheritance

--- OOPS/inheritance.py
+++ OOPS/inheritance.py
@@ -1,3 +1,14 @@
+# Inheritance is a process of obtaining properties and characterstics (variables and methods) of another class. In the hierarchical order,the class which inherits another class is called subclass or child class, and the other classis the parent class   
+
+# Purpose of Inheritance 
+#  OverWriting the method of parent class in a derived class
+
+# In python there are 3 types of inheritance
+#   1. Singlr Inheritance
+#   2. Multiple Inheritance
+#   3. Multilevel Inheritance
+
+
 # Parent class
 class Animal:
     def __init__(self, name):
@@ -23,3 +34,4 @@ cat = Cat("Whiskers")
 # Accessing inherited method and overridden methods
 print(dog.speak())  # Output: Buddy says Woof!
 print(cat.speak())  # Output: Whiskers says Meow!
+
