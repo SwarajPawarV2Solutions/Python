1f7b6bbb9c9ce84b13127972c922414070ee6148
SwarajPawarV2Solutions <swaraj.pawar@v2solutions.com>
2024-10-30 17:23:42 +0530

    Added Theory of Super function

---
+++ OOPS/Super_function.py
@@ -0,0 +1,5 @@
+# Super() function
+
+# The super function is a built-in function in python. It is used to call a method of a parent class from a subclass and super function makes it easier to manage inheritance and avoid duplication of code.
+
+# THe super function can be implemented in single inheritance and multiple inheritance as well. syntax fr suprfunction is as follows: 
