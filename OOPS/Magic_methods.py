40f7a7f355d59b64493d586fa2de3a1b8583203b
SwarajPawarV2Solutions <swaraj.pawar@v2solutions.com>
2024-11-02 23:26:33 +0530

    Added magic_mrthods theory

---
+++ OOPS/Magic_methods.py
@@ -0,0 +1,9 @@
+# Magic methods in python are special methods that allow us to define how instances of a class behave when used with built-in python functions and operators.
+
+# They are also known as dunger(double underscore) methods because their names begin and end with two underscores.
+
+# Some common magic methods in python are as follows :
+
+#    1. __init__, Which is called when an instance of the class is created
+#    2. __str__, which is called when we use str() function on an instance
+#    3. __eq__, which is called when we use the == operator to compare instance.
