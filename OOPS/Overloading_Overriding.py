c89b2c228a8e00ba097c62404b3b34abaeed372b
SwarajPawarV2Solutions <swaraj.pawar@v2solutions.com>
2024-11-02 19:53:53 +0530

    Added theory of Overriding and Overloading

---
+++ OOPS/Overloading_Overriding.py
@@ -0,0 +1,29 @@
+
+# Operator Overloading:
+
+# Overloading is achived using method and operator having different functionalities with same name
+
+# Operator overloading is achived by allowing same operator to have different meanings.
+
+# These operators work for built-in classes. but the same operator behaves differently with different types. 
+
+
+
+# Method Overloading
+
+# Method Overloading can be thought as many methods with same name accepting different numbers of arguments with Same class.
+
+# This should not be confused with polymorphism which requires different classes to have same method name and different functionalities.
+
+# Python by default does not support method Overriding since two methods cannot have the same name in python.
+
+
+
+
+#  Method Overriding
+
+#  Method overriding is useful when we want to modify the behaviour of a methid in  subclass without changing the implementation in the superclass.
+
+# It allows us to reuse code from the superclass while also customizing it to fit our specific needs in the subclass.
+
+# we have already seen method overriding without explicitly mentioning it in inheritance.
